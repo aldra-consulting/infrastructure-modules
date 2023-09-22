5f5dcb6027e3b6872713fa853eb66d0dc58cbbe3
Alexander Zakharov <alexander.zakharov@aldra.no>
2023-09-22 21:04:53 +0200

    feat(route-53): add NS record for each subdomain to main hosted zone

--- packages/route-53/main.tf
+++ packages/route-53/main.tf
@@ -7,8 +7,10 @@ terraform {
 }
 
 locals {
-  domain_name = var.common.project.domain_name
-  tags        = merge(var.account.tags, var.region.tags, var.environment.tags)
+  environment             = var.environment.name
+  domain_name             = var.common.project.domain_name
+  environment_domain_name = local.environment == "production" ? local.domain_name : "${local.environment}.${local.domain_name}"
+  tags                    = merge(var.account.tags, var.region.tags, var.environment.tags)
 }
 
 provider "aws" {}
@@ -18,7 +20,21 @@ provider "aws" {}
 # ------------------------------------------------------------------------------
 
 resource "aws_route53_zone" "this" {
-  name = local.domain_name
+  name = local.environment_domain_name
 
   tags = local.tags
+}
+
+data "aws_route53_zone" "production_route53_zone" {
+  name = local.domain_name
+}
+
+resource "aws_route53_record" "subdomain_ns_record" {
+  count = local.environment == "production" ? 0 : 1
+
+  zone_id = data.aws_route53_zone.production_route53_zone.zone_id
+  name    = aws_route53_zone.this.name
+  type    = "NS"
+  ttl     = "600"
+  records = aws_route53_zone.this.name_servers
 }
