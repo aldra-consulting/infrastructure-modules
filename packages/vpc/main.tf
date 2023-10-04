dd05fd17875ad8f31f005468aaf772d031af4f8e
Alexander Zakharov <alexander.zakharov@aldra.no>
2023-10-04 14:09:18 +0200

    feat(vpc): add VPC endpoints

--- packages/vpc/main.tf
+++ packages/vpc/main.tf
@@ -36,3 +36,47 @@ module "vpc" {
 
   tags = local.tags
 }
+
+module "vpc_endpoints" {
+  source = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
+
+  vpc_id = module.vpc.vpc_id
+
+  create_security_group      = true
+  security_group_name_prefix = "${local.namespace}-vpc-endpoint-"
+  security_group_description = "VPC endpoint security group"
+  security_group_rules = {
+    ingress_https = {
+      description = "HTTPS from VPC"
+      cidr_blocks = [module.vpc.vpc_cidr_block]
+    }
+  }
+
+  endpoints = {
+    s3 = {
+      service         = "s3"
+      route_table_ids = module.vpc.private_route_table_ids
+      tags            = merge(local.tags, { Name = "s3-vpc-endpoint" })
+    },
+    dynamodb = {
+      service         = "dynamodb"
+      service_type    = "Gateway"
+      route_table_ids = module.vpc.private_route_table_ids
+      tags            = merge(local.tags, { Name = "dynamodb-vpc-endpoint" })
+    },
+    ecr_dkr = {
+      service             = "ecr.dkr"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+      tags                = merge(local.tags, { Name = "ecr.dkr-vpc-endpoint" })
+    },
+    ecr_api = {
+      service             = "ecr.api"
+      private_dns_enabled = true
+      subnet_ids          = module.vpc.private_subnets
+      tags                = merge(local.tags, { Name = "ecr.api-vpc-endpoint" })
+    },
+  }
+
+  tags = local.tags
+}
